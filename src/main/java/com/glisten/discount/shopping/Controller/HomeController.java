4537b632ab2f412791e1b06c0e4dac5cbbc98c92
Administrator <315541219@qq.com>
2020-05-07 17:03:22 +0800

    移动端修改，页面跳转问题

--- src/main/java/com/glisten/discount/shopping/Controller/HomeController.java
+++ src/main/java/com/glisten/discount/shopping/Controller/HomeController.java
@@ -1,26 +1,40 @@
 package com.glisten.discount.shopping.Controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.glisten.discount.shopping.Interceptor.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+
+    @Autowired
+    private HttpServletResponse response;
     @Log("电脑端用户登录")
     @RequestMapping("/index")
     public String index(){
         return "home";
     }
 
-    @RequestMapping("/m-index")
-    public String mIndex(){
-        return "m-home";
+
+
+    @RequestMapping("/m-index/{id}")
+    public ModelAndView MI(@PathVariable("id") long id){
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("m-home");
+        mav.addObject("type", id);
+        return mav;
     }
 
     @Log("移动端用户登录")
